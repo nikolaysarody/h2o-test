5e38ca1ca2f28ac0ceb68f0089673b7d826192e6
Nikolay <sarody@ya.ru>
2023-06-05 11:44:51 +0800

    Initial commit

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+import { classNames } from 'shared/lib/helpers/classNames/classNames';
+import './styles/index.scss';
+
+export const App: FC = ({}) => {
+    return (
+        <div className={classNames('app', {}, [])}>
+            <h1>Hello world</h1>
+        </div>
+    );
+}
