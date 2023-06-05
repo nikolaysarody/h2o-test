88f366fc26df68c4b70bb7e431ece818fdc69ad2
Nikolay <sarody@ya.ru>
2023-06-05 18:45:20 +0800

    stylelint setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,10 @@
 import { render } from 'react-dom';
 import { App } from 'app/App';
 import { BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from 'app/providers/ThemeProvider';
 
 render(
     <BrowserRouter>
-        <ThemeProvider>
-            <App />
-        </ThemeProvider>
+        <App />
     </BrowserRouter>,
     document.getElementById('root'),
 );
