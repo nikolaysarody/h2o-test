ea5853a666a5fc9530f81b66823aebca50b56f0b
Nikolay <sarody@ya.ru>
2023-06-06 15:22:31 +0800

    add react testing library

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,6 +1,7 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import { ProgressPlugin, WebpackPluginInstance } from 'webpack';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
 
 export function buildPlugins({ paths }: BuildOptions): WebpackPluginInstance[] {
@@ -12,6 +13,9 @@ export function buildPlugins({ paths }: BuildOptions): WebpackPluginInstance[] {
         new MiniCssExtractPlugin({
             filename: 'css/[name].[contenthash:8].css',
             chunkFilename: 'css/[name].[contenthash:8].css',
+        }),
+        new BundleAnalyzerPlugin({
+            openAnalyzer: false,
         }),
     ];
 }
