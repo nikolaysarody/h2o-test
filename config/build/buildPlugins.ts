6ba91918ede8191c0b12435d8d347548f7d0b715
Nikolay <sarody@ya.ru>
2023-06-07 13:16:45 +0800

    add github actions

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -4,8 +4,8 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
 
-export function buildPlugins({ paths }: BuildOptions): WebpackPluginInstance[] {
-    return [
+export function buildPlugins({ paths, isDev }: BuildOptions): WebpackPluginInstance[] {
+    const plugins = [
         new HtmlWebpackPlugin({
             template: paths.html,
         }),
@@ -14,8 +14,13 @@ export function buildPlugins({ paths }: BuildOptions): WebpackPluginInstance[] {
             filename: 'css/[name].[contenthash:8].css',
             chunkFilename: 'css/[name].[contenthash:8].css',
         }),
-        new BundleAnalyzerPlugin({
-            openAnalyzer: false,
-        }),
     ];
+
+    if (isDev) {
+        plugins.push(new BundleAnalyzerPlugin({
+            openAnalyzer: false,
+        }));
+    }
+
+    return plugins;
 }
