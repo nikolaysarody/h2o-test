e6cb2a0898f1fa0cab372056d6764a3520750842
Nikolay <sarody@ya.ru>
2023-06-07 17:04:11 +0800

    add component render for tests with router

---
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from 'react';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+
+export interface componentRenderOptions {
+    route?: string;
+}
+
+export function componentRender(component: ReactNode, options: componentRenderOptions = {}) {
+    const {
+        route = '/',
+    } = options;
+
+    return render(
+        <MemoryRouter initialEntries={[route]}>
+            {component}
+        </MemoryRouter>,
+    );
+}
