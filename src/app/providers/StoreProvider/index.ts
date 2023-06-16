8f59f9bc8128d64e83e22da52f68edb9e0b5f36d
Nikolay <sarody@ya.ru>
2023-06-16 20:12:51 +0800

    ts strict enable

--- src/app/providers/StoreProvider/index.ts
+++ src/app/providers/StoreProvider/index.ts
@@ -1,3 +1,3 @@
-export { createReduxStore } from 'app/providers/StoreProvider/config/store';
+export { createReduxStore, AppDispatch } from 'app/providers/StoreProvider/config/store';
 export { StoreProvider } from 'app/providers/StoreProvider/ui/StoreProvider';
 export type { StateSchema } from './config/StateSchema';
